762dd9b73786213bf4786e3019e838e642a8adc3
Jakob Grabner <jakob.grabner@runtastic.com>
2021-12-02 08:56:09 +0100

    Improve day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,30 +1,28 @@
 fun main() {
-    fun part1(input: List<String>) {
-        println(
-            input.windowed(size = 2) { (a, b) ->
-//            println("$a, $b -> ${b > a}")
-                b > a
-            }.count { it }
-        )
-    }
-
-    fun part2(input: List<String>) {
-        println(
-            input
-                .map { it.toInt() }
-                .windowed(size = 3) { w ->
-                    w.sum()
-                }.windowed(size = 2) { (a, b) ->
-//                    println("$a, $b -> ${b > a}")
-                    b > a
-                }.count { it }
-        )
-    }
-
 
     val input = readInput("day01")
     part1(input)
     println("----------")
     part2(input)
 
+}
+
+private fun part1(input: List<String>) {
+    var result = 0
+    input
+        .map { it.toInt() }
+        .reduce { prev, curr -> if (curr > prev) result++; curr }
+    println(result)
+}
+
+private fun part2(input: List<String>) {
+    var result = 0
+
+    input
+        .map { it.toInt() }
+        .windowed(size = 3) { w ->
+            w.sum()
+        }.reduce { prev, curr -> if (curr > prev) result++; curr }
+
+    println(result)
 }
